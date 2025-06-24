501e353921a2b9d9f4e4415d320727bfb37310fe
Zongze Wu <astrowu61@gmail.com>
2025-06-23 21:11:51 -0700

    Axios setup

---
+++ frontend/src/utils/api.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+// create an instance of axios
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// request interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error)=> {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error)=> {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+export default api;
